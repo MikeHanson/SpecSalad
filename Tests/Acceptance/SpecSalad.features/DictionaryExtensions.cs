08455f3cc66062847d4100cf7c0a23efd2dec553
mike.hanson <mike.hanson@hotels4u.com>
2014-10-01 14:13:32 +0100

    Moved DictionaryExtensions from main project

--- Tests/Acceptance/SpecSalad.features/DictionaryExtensions.cs
+++ Tests/Acceptance/SpecSalad.features/DictionaryExtensions.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace SpecSalad
+namespace SpecSalad.features
 {
     public static class DictionaryExtensions
     {
+        public static IDictionary<string, string> Subset(this IEnumerable<KeyValuePair<string, string>> source, params string[] keys)
+        {
+            return source.Where(item => keys.Contains(item.Key))
+                         .ToDictionary(item => item.Key, item => item.Value);
+        }
+
         public static string DefaultValue(this IEnumerable<KeyValuePair<string, string>> source)
         {
             var valuePairs = (from i in source select i).ToList();
